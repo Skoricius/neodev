3ef4ad6c9e7c43aafcf87a97ffd7f390238ec10d
Skoricius <lukaskoric32@gmail.com>
2024-03-17 01:23:17 +0000

    telescope styling

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -5,3 +5,17 @@ vim.keymap.set('n', '<leader>fs', function()
 	builtin.grep_string({ search = vim.fn.input("Grep > ") })
 end)
 vim.keymap.set('n', '<leader>vh', builtin.help_tags, {})
+require('telescope').setup{
+     defaults = {
+        git_worktrees = vim.g.git_worktrees,
+        path_display = { "truncate" },
+        sorting_strategy = "ascending",
+        layout_config = {
+          horizontal = { prompt_position = "top", preview_width = 0.55 },
+          vertical = { mirror = false },
+          width = 0.87,
+          height = 0.80,
+          preview_cutoff = 120,
+        },
+      },
+    }
