888b6dc8af15d23f9e7ba7541f64494063870ca0
Skoricius <lukaskoric32@gmail.com>
2024-03-17 08:53:30 +0000

    adding neotree

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -2,20 +2,20 @@ local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
 vim.keymap.set('n', '<C-p>', builtin.git_files, {})
 vim.keymap.set('n', '<leader>fs', function()
-	builtin.grep_string({ search = vim.fn.input("Grep > ") })
+    builtin.grep_string({ search = vim.fn.input("Grep > ") })
 end)
 vim.keymap.set('n', '<leader>vh', builtin.help_tags, {})
-require('telescope').setup{
-     defaults = {
+require('telescope').setup {
+    defaults = {
         git_worktrees = vim.g.git_worktrees,
         path_display = { "truncate" },
         sorting_strategy = "ascending",
         layout_config = {
-          horizontal = { prompt_position = "top", preview_width = 0.55 },
-          vertical = { mirror = false },
-          width = 0.87,
-          height = 0.80,
-          preview_cutoff = 120,
+            horizontal = { prompt_position = "top", preview_width = 0.55 },
+            vertical = { mirror = false },
+            width = 0.87,
+            height = 0.80,
+            preview_cutoff = 120,
         },
-      },
-    }
+    },
+}
