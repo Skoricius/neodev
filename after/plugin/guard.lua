00c0a7a835ec387042f1c9d89e384f8521764d83
Skoricius <lukaskoric32@gmail.com>
2024-03-19 00:12:25 +0000

    adding vscode fix for guard

--- after/plugin/guard.lua
+++ after/plugin/guard.lua
@@ -1,17 +1,19 @@
-local ft = require('guard.filetype')
+if vim.g.vscode == nil then
+    local ft = require('guard.filetype')
 
--- Assuming you have guard-collection
-ft('python'):fmt('black')
-    :append('isort')
-    :lint('pylint')
+    -- Assuming you have guard-collection
+    ft('python'):fmt('black')
+        :append('isort')
+        :lint('pylint')
 
--- Call setup() LAST!
-require('guard').setup({
-    -- Choose to format on every write to a buffer
-    fmt_on_save = true,
-    -- Use lsp if no formatter was defined for this filetype
-    lsp_as_default_formatter = false,
-    -- By default, Guard writes the buffer on every format
-    -- You can disable this by setting:
-    -- save_on_fmt = false,
-})
+    -- Call setup() LAST!
+    require('guard').setup({
+        -- Choose to format on every write to a buffer
+        fmt_on_save = true,
+        -- Use lsp if no formatter was defined for this filetype
+        lsp_as_default_formatter = false,
+        -- By default, Guard writes the buffer on every format
+        -- You can disable this by setting:
+        -- save_on_fmt = false,
+    })
+end
