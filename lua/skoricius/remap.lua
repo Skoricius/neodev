76e6de94d2b51086d12b7e905ef312245bc7699a
skoricius <lukaskoric32@gmail.com>
2024-04-11 20:03:39 +0100

    indenting in visual mode

--- lua/skoricius/remap.lua
+++ lua/skoricius/remap.lua
@@ -10,6 +10,8 @@ vim.keymap.set("n", "<C-u>", "<C-u>zz")
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
+vim.keymap.set("v", ">", ">gv")
+vim.keymap.set("v", "<", "<gv")
 
 vim.keymap.set("n", "<leader>vwm", function()
     require("vim-with-me").StartVimWithMe()
