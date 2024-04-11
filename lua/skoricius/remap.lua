4664ee588d660ab95441095520dc04a67b750d2c
Skoricius <lukaskoric32@gmail.com>
2024-04-11 16:50:34 +0100

    mapping quit

--- lua/skoricius/remap.lua
+++ lua/skoricius/remap.lua
@@ -58,5 +58,6 @@ vim.keymap.set("n", "<C-l>", "<C-w>l")
 
 -- saving and quitting files
 vim.keymap.set("n", "<C-s>", "<cmd>w!<cr>")
-vim.keymap.set("n", "<C-q>", "<C-w>c")
+vim.keymap.set("n", "<C-c>", "<C-w>c")
+vim.keymap.set("n", "<C-q>", "<C-w>qa")
 
