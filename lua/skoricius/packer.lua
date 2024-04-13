103129729273c6745ebe2010b2dd9b4b754449e2
Skoricius <lukaskoric32@gmail.com>
2024-04-13 11:19:52 +0000

    magma instead of molten

--- lua/skoricius/packer.lua
+++ lua/skoricius/packer.lua
@@ -196,8 +196,13 @@ return require('packer').startup(function(use)
         disable = vscode_cond
     }
     -- Plugin for interactively running Python scripts
-    use { 'dccsillag/magma-nvim',
-        run = ':UpdateRemotePlugins',
-        disable = vscode_cond
+    use {
+        "benlubas/molten-nvim",
+        version = "^1.0.0", -- use version <2.0.0 to avoid breaking changes
+        build = ":UpdateRemotePlugins",
+        init = function()
+            -- this is an example, not a default. Please see the readme for more configuration options
+            vim.g.molten_output_win_max_height = 12
+        end,
     }
 end)
