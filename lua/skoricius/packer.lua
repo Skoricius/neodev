80b6b4a87c05dbbb2297436dd5e51b3bac896c78
Skoricius <lukaskoric32@gmail.com>
2024-04-13 18:02:21 +0000

    switching to iron

--- lua/skoricius/packer.lua
+++ lua/skoricius/packer.lua
@@ -196,13 +196,14 @@ return require('packer').startup(function(use)
         disable = vscode_cond
     }
     -- Plugin for interactively running Python scripts
-    use {
-        "benlubas/molten-nvim",
-        version = "^1.0.0", -- use version <2.0.0 to avoid breaking changes
-        build = ":UpdateRemotePlugins",
-        init = function()
-            -- this is an example, not a default. Please see the readme for more configuration options
-            vim.g.molten_output_win_max_height = 12
-        end,
-    }
+    -- use {
+    --     "benlubas/molten-nvim",
+    --     version = "^1.0.0", -- use version <2.0.0 to avoid breaking changes
+    --     build = ":UpdateRemotePlugins",
+    --     init = function()
+    --         -- this is an example, not a default. Please see the readme for more configuration options
+    --         vim.g.molten_output_win_max_height = 12
+    --     end,
+    -- }
+    use { 'Vigemus/iron.nvim' }
 end)
