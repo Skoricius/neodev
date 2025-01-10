a3258ee56bc35c2fee2d753ac3123ec9bc9edd1c
Skoricius <lukaskoric32@gmail.com>
2025-01-10 16:08:32 +0000

    removing old plugins

--- lua/old_plugins/diffview.lua
+++
@@ -1,5 +0,0 @@
-if vim.g.vscode == nil then
-    vim.keymap.set("n", "<leader>Do", "<cmd>DiffviewOpen<cr>")
-    vim.keymap.set("n", "<leader>Dc", "<cmd>DiffviewClose<cr>")
-    vim.keymap.set("n", "<leader>Dh", "<cmd>DiffviewFileHistory %<cr>")
-end
--- lua/old_plugins/remote_copy.lua
+++
@@ -1,9 +0,0 @@
-vim.keymap.set('n', '<leader>c', require('osc52').copy_operator, {expr = true})
-vim.keymap.set('n', '<leader>cc', '<leader>c_', {remap = true})
-vim.keymap.set('v', '<leader>c', require('osc52').copy_visual)
-require('osc52').setup {
-  max_length = 0,           -- Maximum length of selection (0 for no limit)
-  silent = false,           -- Disable message on successful copy
-  trim = true,             -- Trim surrounding whitespaces before copy
-  tmux_passthrough = true, -- Use tmux passthrough (requires tmux: set -g allow-passthrough on)
-}
